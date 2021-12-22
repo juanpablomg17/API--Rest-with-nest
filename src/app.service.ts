550f62b834b546b5c4524beb7fa707f4ff52560c
fran_richards <juanpablomg17@gmail.com>
2021-12-22 15:48:53 -0500

    testing a log

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,11 +6,12 @@ import config from '../config';
 export class AppService {
   constructor(
     @Inject(config.KEY) private configEnv: ConfigType<typeof config>,
-  ) {}
+  ) { }
 
   getHello(): string {
     const apiKey = this.configEnv.apiKey;
     const dataBaseName = this.configEnv.database;
+    console.log('holas')
     return `Hello World! ${apiKey} and ${dataBaseName}`;
   }
 }
